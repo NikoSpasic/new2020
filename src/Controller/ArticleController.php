f5184d58101081aefcf5daa87277a1652ca076b6
NikoSpasic <nikola@spasic.dev>
2020-01-26 20:10:22 +0100

    maker, doctrine (1-5)

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage()
+    {
+        return new Response('BlaBlaBla');
+    }
+}
